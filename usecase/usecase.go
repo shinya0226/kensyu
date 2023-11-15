67dec7116e34f65028cf0f9c125c55bf49de1d0e
shinya0226 <79456274+shinya0226@users.noreply.github.com>
2023-11-15 11:34:36 +0900

    Create usecase.go

---
+++ usecase/usecase.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// パスワードの暗号化（DBからパスワードを取り出す時に使用）
+func HashPassword(rawPassword string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(rawPassword), bcrypt.DefaultCost)
+	return string(hashedPassword), err
+}
+
+// 暗号化されたパスワードとユーザーが入力したパスワードの比較
+func VerifyPassword(hashedPassword string, entryPassword string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(entryPassword))
+	return err
+}
+
+// JWTの発行
+func CreateToken(email string) (string, error) {
+	//tokenの作成
+	token := jwt.New(jwt.GetSigningMethod("HS256"))
+	//Claimsの設定
+	token.Claims = jwt.MapClaims{
+		"user": email,
+		"exp":  time.Now().Add(time.Hour * 1).Unix(), //1時間の有効期限を設定
+	}
+	//署名
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	var secretKey = JWT_SECRET
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return string(http.StatusInternalServerError), err
+
+	}
+	return tokenString, err
+
+}
+
+// JWTの検証
+func VerifyToken(tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		return token, err
+	}
+	return token, err
+}
