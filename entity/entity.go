ee4fa8cb1bff6a3c5a5c427c5124007c1dfda723
shinya0226 <79456274+shinya0226@users.noreply.github.com>
2023-11-15 11:27:48 +0900

    Create entity.go

---
+++ entity/entity.go
@@ -0,0 +1,10 @@
+package handler
+
+type (
+	User struct {
+		Email    string `json:"Email" form:"Email"`
+		Password string `json:"Password" form:"Password"`
+		Name     string `json:"Name"`
+		IsAdmin  int    `json:"IsAdmin"`
+	}
+)
