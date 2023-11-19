cf764d12fe0fe41eb31440b633b6b345b0f91443
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-11-19 14:04:34 +0900

    file-setup modified

--- server/main.go
+++ server/main.go
@@ -1,8 +1,6 @@
-package server
+package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/shinya0226/kensyu/handler"
@@ -17,16 +15,11 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// ルートを設定
-	e.GET("/", Hello)
+	e.GET("/", handler.Hello)
 
 	e.POST("/login", handler.Login)
 
 	// サーバーをポート番号8080で起動
 	e.Logger.Fatal(e.Start(":8080"))
 
-}
-
-// 初期画面の表示
-func Hello(c echo.Context) error {
-	return c.String(http.StatusOK, "お仕事おつかれ様")
 }
