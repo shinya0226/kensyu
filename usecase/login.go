038cdc15830c9eab8f135938180c9ecb6832edff
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-11-26 17:17:29 +0900

    usecase_file added

--- usecase/login.go
+++ usecase/login.go
@@ -1,6 +1,8 @@
 package usecase
 
-import "github.com/shinya0226/kensyu/entity"
+import (
+	"github.com/shinya0226/kensyu/entity"
+)
 
 type loginUsecase struct {
 	repo entity.IUserRepository
@@ -11,27 +13,44 @@ func NewLoginUsecase(repo entity.IUserRepository) ILoginUsecase {
 }
 
 type ILoginUsecase interface {
-	Login(e entity.User) (string, error)
+	Login(e entity.User) (LoginFormat, error)
 }
 
-func (u *loginUsecase) Login(e entity.User) (string, error) {
+type LoginFormat struct {
+	email        string
+	name         string
+	isAdmin      int
+	access_token string
+}
+
+func (u *loginUsecase) Login(e entity.User) (LoginFormat, error) {
 	//該当するユーザーを抽出（found）
 	found, err := u.repo.FindSingleRow(e.Email)
 
+	//出力の型を定義
+	logfo := LoginFormat{}
+
+	logfo.email = found.Email
+	logfo.name = found.Name
+	logfo.isAdmin = found.IsAdmin
+
 	if err != nil {
-		return "", err
+		return logfo, err
 	}
 	//DBのパスワードのハッシュ化
 	pass, err := HashPassword(e.Password)
 	if err != nil {
-		return "", err
+		return logfo, err
 	}
 	//パスワードの比較
 	if ans := VerifyPassword(pass, found.Password); ans != nil {
-		return "", err
+		return logfo, err
 	}
 	//JWTの作成
-	message, err := CreateToken(e.Email)
-	return message, err
+	jwt_message, err := CreateToken(e.Email)
+	//出力の型を定義
+	logfo.access_token = jwt_message
+
+	return logfo, err
 
 }
