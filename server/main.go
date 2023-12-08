089ce12275393c7d1eeac14a897ab0c976653fa9
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-08 11:24:11 +0900

    littlt erased

--- server/main.go
+++ server/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"os"
-
+	"github.com/golang-jwt/jwt"
 	echojwt "github.com/labstack/echo-jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -31,8 +30,18 @@ func main() {
 	//アカウント一覧取得処理
 	// e.POST("/accounts/:page", handler.GetAccounts())
 	r := e.Group("/restricted")
-	r.Use(echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))))
-	r.POST("", handler.Restricted())
+
+	config := echojwt.Config{
+		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+			return new(jwtCustomClaims)
+		},
+		SigningKey: []byte("secret"),
+	}
+	r.Use(echojwt.WithConfig(config))
+	r.GET("", restricted)
+
+	// r.Use(echojwt.JWT([]byte(os.Getenv("JWT_SECRET"))))
+	// r.POST("", handler.Restricted())
 
 	// サーバーをポート番号8080で起動
 	e.Logger.Fatal(e.Start(":8080"))
