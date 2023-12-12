3b01b495ee5ab3dce3e47957e72cd2cb859e627d
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-12 10:09:11 +0900

    migration fixed

--- migrations/users/000001_create_users_table.up.sql
+++ migrations/users/000001_create_users_table.up.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS users(
     Email varchar(255) NOT NULL PRIMARY KEY,
-    Password varchar(255) NOT NULL
+    Password varchar(255) NOT NULL,
+    Name varchar(255) NOT NULL,
+    IsAdmin int NOT NULL
 );
