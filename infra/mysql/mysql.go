78833b3e00a596fc7181c285479bbb9103de0bf7
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-13 11:30:42 +0900

    mdf

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -19,9 +19,9 @@ func ConnectionDB() *sql.DB {
 	//　DB読み込み
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbPort, dbName)
 	db, err := sql.Open("mysql", dsn)
+	db.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.Close()
 	return db
 }
