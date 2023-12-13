be9b0c02b7d6e6d8605d2f1dc5c925203d59d3fd
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-13 12:11:24 +0900

    mdf

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -2,23 +2,23 @@ package mysql
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"os"
 )
 
 // DBに接続
 func ConnectionDB() *sql.DB {
 	//　環境変数の設定
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASS")
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	// dbUser := os.Getenv("DB_USER")
+	// dbPass := os.Getenv("DB_PASS")
+	// dbHost := os.Getenv("DB_HOST")
+	// dbPort := os.Getenv("DB_PORT")
+	// dbName := os.Getenv("DB_NAME")
 
 	//　DB読み込み
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbPort, dbName)
-	db, err := sql.Open("mysql", dsn)
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPass, dbHost, dbPort, dbName)
+	// db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", "root:Shinya0023@tcp(127.0.0.1:3306)/yamamoto?parseTime=true")
+
 	if err != nil {
 		log.Fatal(err)
 	}
