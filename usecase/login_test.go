ec67999c93f5077c6508ada90c7131c63d8e179f
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-13 14:06:17 +0900

    mdf

--- usecase/login_test.go
+++ usecase/login_test.go
@@ -60,10 +60,11 @@ func TestLogin(t *testing.T) {
 	for _, tt := range testCase {
 		t.Run(tt.Description, func(t *testing.T) {
 			db := ConnectionDB()
-			db.Close()
+
 			userRepo := NewUserRepository(db)
 			loginUsecase := usecase.NewLoginUsecase(userRepo)
 			got, err := loginUsecase.Login(entity.User(tt.Entity))
+			db.Close()
 
 			//　errがあるか判別（あるときはtrue,ないときはfalse）
 			if (err != nil) != tt.WantErr {
