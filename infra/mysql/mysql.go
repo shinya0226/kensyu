02a94b0e47fa4222b0e58206e9ebb4114c1c5e6e
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-14 12:58:37 +0900

    db change

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -2,12 +2,13 @@ package mysql
 
 import (
 	"database/sql"
+	"log"
 )
 
 func ConnectionDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:Shinya0023@tcp(127.0.0.1:3306)/yamamoto?parseTime=true")
+	db, err := sql.Open("mysql", "root:Shinya0023@tcp(127.0.0.1:3306)/test_fix?parseTime=true")
 	if err != nil {
-		return db
+		log.Fatal(err)
 	}
 	return db
 }
