7cd73f834302058c4bc02ac601492879746c8869
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-14 12:34:44 +0900

    first

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -2,13 +2,12 @@ package mysql
 
 import (
 	"database/sql"
-	"log"
 )
 
 func ConnectionDB() *sql.DB {
 	db, err := sql.Open("mysql", "root:Shinya0023@tcp(127.0.0.1:3306)/yamamoto?parseTime=true")
 	if err != nil {
-		log.Fatal(err)
+		return db
 	}
 	return db
 }
