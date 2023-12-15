881f7510022e503607625494cf2963cd0671b1f8
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-15 10:16:10 +0900

    enhigh coverage

--- handler/handler.go
+++ handler/handler.go
@@ -12,7 +12,25 @@ import (
 // ログイン処理（機能）
 func Login(u usecase.ILoginUsecase) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return LoginWithUsecase(u, c)
+		eu := new(entity.User)
+		logfo := LoginFormat{}
+
+		if err := c.Bind(eu); err != nil {
+			return err
+		}
+		//　Loginの出力をmessageに格納（修正）
+		message, err := u.Login(*eu)
+		if err != nil {
+			return err
+		}
+		//　formatに追加
+		logfo.Email = message.Email
+		logfo.Name = message.Name
+		logfo.IsAdmin = message.IsAdmin
+		logfo.AccessToken = message.AccessToken
+
+		return c.JSON(http.StatusOK, logfo) //　structに詰める
+
 	}
 }
 
