480277461dbd83c6d8eb6ecb13b1c90c89f5c823
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-15 10:29:31 +0900

    enhigh coverage

--- handler/handler.go
+++ handler/handler.go
@@ -6,23 +6,22 @@ import (
 	"github.com/shinya0226/kensyu/entity"
 	"github.com/shinya0226/kensyu/usecase"
 
-	"fmt"
-
 	"github.com/labstack/echo/v4"
 )
-
-// ログイン処理（機能）
-func Login(u usecase.ILoginUsecase) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		return LoginWithUsecase(u, c)
-	}
-}
 
 type LoginFormat struct {
 	Email       string `json:"email"`
 	Name        string `json:"name"`
 	IsAdmin     int    `json:"isAdmin"`
 	AccessToken string `json:"access_token"`
+}
+
+// ログイン処理（機能）
+func Login(u usecase.ILoginUsecase) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// return LoginWithUsecase(u, c)
+		return c.String(http.StatusOK, "ok")
+	}
 }
 
 // ログイン処理（詳細）
@@ -39,15 +38,12 @@ func LoginWithUsecase(u usecase.ILoginUsecase, c echo.Context) error {
 		return err
 	}
 	//　formatに追加
-	fmt.Println(logfo)
-	fmt.Println(message)
-	// logfo.Email = message.Email
-	// logfo.Name = message.Name
-	// logfo.IsAdmin = message.IsAdmin
-	// logfo.AccessToken = message.AccessToken
+	logfo.Email = message.Email
+	logfo.Name = message.Name
+	logfo.IsAdmin = message.IsAdmin
+	logfo.AccessToken = message.AccessToken
 
-	// return c.JSON(http.StatusOK, logfo) //　structに詰める
-	return err
+	return c.JSON(http.StatusOK, logfo) //　structに詰める
 }
 
 func LoginAccount(u usecase.ILoginUsecase) echo.HandlerFunc {
