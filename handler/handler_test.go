fb4839bbea1d54780426c33b154c6d537c2ae92e
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-15 14:58:50 +0900

    enhigh coverage

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -19,55 +19,55 @@ func TestLogin(t *testing.T) {
 		Name     string `json:"name"`
 		IsAdmin  int    `json:"isAdmin"`
 	}
-	type LoginFormatHandler struct {
+	type LoginFormat struct {
 		Email       string `json:"email"`
 		Name        string `json:"name"`
 		IsAdmin     int    `json:"isAdmin"`
 		AccessToken string `json:"access_token"`
 	}
 	testCase := []struct {
-		Description string             `json:"Description"`
-		Entity      user               `json:"Email"` //　入力
-		Want        LoginFormatHandler //　出力
-		WantErr     bool               //　エラーが出るときはtrue
+		Description string      `json:"Description"`
+		Entity      user        `json:"Email"` //　入力
+		Want        LoginFormat //　出力
+		WantErr     bool        //　エラーが出るときはtrue
 	}{
 		{
 			Description: "EmailとPasswordが両方合致",
 			Entity:      user{"shinya.yamamoto6@persol-pt.co.jp", "yamamo10", "山本真也", 0},
-			Want:        LoginFormatHandler{"shinya.yamamoto6@persol-pt.co.jp", "山本真也", 0, "Anything"},
+			Want:        LoginFormat{"shinya.yamamoto6@persol-pt.co.jp", "山本真也", 0, "Anything"},
 			WantErr:     false,
 		},
 		{
 			Description: "Emailエラーによる不合致",
 			Entity:      user{"Emailは違うよ", "yamamo10", "山本真也", 0},
-			Want:        LoginFormatHandler{"", "", 0, ""},
+			Want:        LoginFormat{"", "", 0, ""},
 			WantErr:     true,
 		},
 		{
 			Description: "Passwordエラーによる不合致",
 			Entity:      user{"shinya.yamamoto6@persol-pt.co.jp", "Passwordは違うよ", "山本真也", 0},
-			Want:        LoginFormatHandler{"shinya.yamamoto6@persol-pt.co.jp", "", 0, ""},
+			Want:        LoginFormat{"shinya.yamamoto6@persol-pt.co.jp", "", 0, ""},
 			WantErr:     true,
 		},
 		{
 			Description: "Nothingエラーによる不合致",
 			Entity:      user{"", "", "山本真也", 0},
-			Want:        LoginFormatHandler{"", "", 0, ""},
+			Want:        LoginFormat{"", "", 0, ""},
 			WantErr:     true,
 		},
 	}
-	// var userEntity = entity.User{
-	// 	Email:    "shinya.yamamoto6@persol-pt.co.jp",
-	// 	Password: "yamamo10",
-	// 	Name:     "山本真也",
-	// 	IsAdmin:  0}
+	var userEntity = entity.User{
+		Email:    "shinya.yamamoto6@persol-pt.co.jp",
+		Password: "yamamo10",
+		Name:     "山本真也",
+		IsAdmin:  0}
 
-	// // Login()の出力
-	// var userResponse = usecase.LoginFormat{
-	// 	Email:       "shinya.yamamoto6@persol-pt.co.jp",
-	// 	Name:        "山本真也",
-	// 	IsAdmin:     0,
-	// 	AccessToken: "Anything"}
+	// Login()の出力
+	var userResponse = LoginFormat{
+		Email:       "shinya.yamamoto6@persol-pt.co.jp",
+		Name:        "山本真也",
+		IsAdmin:     0,
+		AccessToken: "Anything"}
 
 	for _, tt := range testCase {
 		t.Run(tt.Description, func(t *testing.T) {
@@ -75,7 +75,7 @@ func TestLogin(t *testing.T) {
 			// defer ctrl.Finish()
 			//　mockの生成
 			testMock := handler.NewMockILoginUsecase(ctrl)
-			testMock.EXPECT().Login(tt.Entity).Return(tt.Want, nil)
+			testMock.EXPECT().Login(userEntity).Return(userResponse, nil)
 			// handler.Login(testMock)
 			got, err := testMock.Login(entity.User(tt.Entity))
 			//　errがあるか判別（あるときはtrue,ないときはfalse）
