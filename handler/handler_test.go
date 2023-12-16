82ecf4767198aea524bae83b29104b5bc9a07ad5
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-16 15:59:30 +0900

    enhigh coverage

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -102,3 +102,32 @@ func LoginFunc(u usecase.ILoginUsecase) echo.HandlerFunc {
 		return handler.LoginWithUsecase(u, c)
 	}
 }
+
+// ログイン処理（詳細）
+func LoginWithUsecase(u usecase.ILoginUsecase, c echo.Context) error {
+	eu := new(entity.User)
+	logfo := LoginFormat{}
+
+	if err := c.Bind(eu); err != nil {
+		return err
+	}
+	//　Loginの出力をmessageに格納（修正）
+	message, err := u.Login(*eu)
+	if err != nil {
+		return err
+	}
+	//　formatに追加
+	logfo.Email = message.Email
+	logfo.Name = message.Name
+	logfo.IsAdmin = message.IsAdmin
+	logfo.AccessToken = message.AccessToken
+
+	return c.JSON(http.StatusOK, logfo) //　structに詰める
+}
+
+type LoginFormat struct {
+	Email       string `json:"email"`
+	Name        string `json:"name"`
+	IsAdmin     int    `json:"isAdmin"`
+	AccessToken string `json:"access_token"`
+}
