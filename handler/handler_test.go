1ac298e3b8c5cf7239e2f36a43f64131d313bcd3
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-17 13:41:54 +0900

    enhigh coverage

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,10 +1,8 @@
 package handler_test
 
 import (
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"strings"
 	"testing"
 
@@ -12,17 +10,11 @@ import (
 	gomock "github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/stretchr/testify/assert"
 
 	"github.com/shinya0226/kensyu/entity"
 	"github.com/shinya0226/kensyu/handler"
 	"github.com/shinya0226/kensyu/usecase"
 )
-
-func TestMain(m *testing.M) {
-	code := m.Run()
-	os.Exit(code)
-}
 
 // handler_testの実行
 func TestLogin(t *testing.T) {
@@ -74,85 +66,27 @@ func TestLogin(t *testing.T) {
 			defer ctrl.Finish()
 			//　mockの生成
 			testMock := handler.NewMockILoginUsecase(ctrl)
+
 			testMock.EXPECT().Login(userEntity).Return(userResponse, nil)
-			got, err := testMock.Login(userEntity)
-			if (err != nil) != tt.WantErr {
-				t.Errorf("Login() error = %v,wantErr = %v", err, tt.WantErr)
-			}
-			assert.Equal(t, tt.Want.Email, got.Email)
-			assert.Equal(t, tt.Want.Name, got.Name)
-			assert.Equal(t, tt.Want.AccessToken, got.AccessToken)
 
-		})
-	}
-}
-
-func TestUsecase(t *testing.T) {
-	// Login()の入力
-	type user struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-		Name     string `json:"name"`
-		IsAdmin  int    `json:"isAdmin"`
-	}
-	type LoginFormat struct {
-		Email       string `json:"email"`
-		Name        string `json:"name"`
-		IsAdmin     int    `json:"isAdmin"`
-		AccessToken string `json:"access_token"`
-	}
-	testCase := []struct {
-		Description string      `json:"description"`
-		Entity      user        `json:"user"` //　入力
-		Want        LoginFormat //　出力
-		WantErr     bool        //　エラーが出るときはtrue
-	}{
-		{
-			Description: "EmailとPasswordが両方合致",
-			Entity:      user{"shinya.yamamoto6@persol-pt.co.jp", "yamamo10", "山本真也", 0},
-			Want:        LoginFormat{"shinya.yamamoto6@persol-pt.co.jp", "山本真也", 0, "Anything"},
-			WantErr:     false,
-		},
-	}
-	var userEntity = entity.User{
-		Email:    "shinya.yamamoto6@persol-pt.co.jp",
-		Password: "yamamo10",
-		Name:     "山本真也",
-		IsAdmin:  0}
-
-	// Login()の出力
-	var userResponse = usecase.LoginFormat{
-		Email:       "shinya.yamamoto6@persol-pt.co.jp",
-		Name:        "山本真也",
-		IsAdmin:     0,
-		AccessToken: "Anything"}
-
-	for _, tt := range testCase {
-		t.Run(tt.Description, func(t *testing.T) {
-			e := echo.New()
-			e.Use(middleware.Logger())
-			e.Use(middleware.Recover())
-			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
-			//　mockの生成
-			testMock := handler.NewMockILoginUsecase(ctrl)
-			testMock.EXPECT().Login(userEntity).Return(userResponse, nil)
+			// next := func(c echo.Context) error {
+			// 	return handler.LoginWithUsecase(testMock, c)
+			// }
+			// handler.LoginFunc(testMock)
+			// handler.LoginWithUsecase(testMock, c)
 
 			req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(""))
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
-			//　error
-			err := LoginWithUsecase(testMock, c)
-			if err != nil {
-				log.Fatal(err)
-			}
+
+			Login(testMock)
+			LoginWithUsecase(testMock, c)
 		})
 	}
 }
-
 func Login(u usecase.ILoginUsecase) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return LoginWithUsecase(u, c)
+		return handler.LoginWithUsecase(u, c)
 	}
 }
 
