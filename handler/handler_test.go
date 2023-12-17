51a025fce39713d8d5759f5b30558244b6a829b2
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-17 17:46:36 +0900

    enhigh coverage

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -135,7 +135,7 @@ func TestLoginWithUsecase(t *testing.T) {
 		AccessToken: "Anything"}
 
 	var (
-		userJSON = `{email":"shinya.yamamoto6@persol-pt.co.jp","password":"yamamo10", "name":"山本真也","isAdmin":0}`
+		userJSON = `{"email":"shinya.yamamoto6@persol-pt.co.jp","password":"yamamo10", "name":"山本真也","isAdmin":0}`
 	)
 	e := echo.New()
 	ctrl := gomock.NewController(t)
