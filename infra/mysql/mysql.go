7194329b3048944b1f13f48f9df63fa5583046f0
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-18 12:54:59 +0900

    enhigh coverage

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -7,7 +7,6 @@ import (
 func ConnectionDB() *sql.DB {
 	db, err := sql.Open("mysql", "atsuser:atspass@tcp(localhost:3306)/kensyu_testing?parseTime=true")
 	if err != nil {
-		// log.Fatal(err)
 		return db
 	}
 	return db
