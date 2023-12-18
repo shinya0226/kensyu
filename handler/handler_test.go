eb0f5fa268e430c8d52de7c79eaec9e859f1ff99
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-18 13:32:08 +0900

    enhigh coverage

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -95,6 +95,12 @@ func TestLoginWithUsecase(t *testing.T) {
 			Want:        `{"email":"shinya.yamamoto6@persol-pt.co.jp","name":"山本真也","isAdmin":0,"access_token":"Anything"}`,
 			WantErr:     false,
 		},
+		{
+			Description: "Emailエラーによる不合致",
+			Entity:      `{"email":"Emailは違うよ","password":"yamamo10","name":"山本真也","isAdmin":0}`,
+			Want:        `{"email":"shinya.yamamoto6@persol-pt.co.jp","name":"山本真也","isAdmin":0,"access_token":"Anything"}`,
+			WantErr:     false,
+		},
 	}
 	var userEntity = entity.User{
 		Email:    "shinya.yamamoto6@persol-pt.co.jp",
