1c0a2e712cbe76e74cde9db556e36fecf04cd11f
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-19 11:26:15 +0900

    enhigh coverage

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -24,9 +24,6 @@ func CreateToken(email string) (string, error) {
 		"exp":  time.Now().Add(time.Hour * 1).Unix(), //　1時間の有効期限を設定
 	}
 	//　署名
-	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-	if err != nil {
-		return "", err
-	}
+	tokenString, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 	return tokenString, nil
 }
