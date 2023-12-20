4c8807892384797a188fb901b1647f4f406f3b48
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-20 16:50:36 +0900

    enhigh coverage

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -2,20 +2,17 @@ package mysql
 
 import (
 	"database/sql"
-	"fmt"
 )
 
 func ConnectionDB() *sql.DB {
 	//　環境変数の定義
-	//user := os.Getenv("DB_USER")
-	//pass := os.Getenv("DB_PASS")
-	//host := os.Getenv("DB_HOST")
-	//port := os.Getenv("DB_PORT")
-	//name := os.Getenv("DB_NAME")
+	//　user := os.Getenv("DB_USER")
+	//　pass := os.Getenv("DB_PASS")
+	//　host := os.Getenv("DB_HOST")
+	//　port := os.Getenv("DB_PORT")
+	//　name := os.Getenv("DB_NAME")
 	//　DB読み込み
 	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
-	dsn := fmt.Sprintf("atsuser:atspass@tcp(localhost:3306)/kensyu_testing?parseTime=true")
-
-	db, _ := sql.Open("mysql", dsn)
+	db, _ := sql.Open("mysql", "atsuser:atspass@tcp(localhost:3306)/kensyu_testing?parseTime=true")
 	return db
 }
