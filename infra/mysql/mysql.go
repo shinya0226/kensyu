cdc5238681091b13b241122112ac4cb43a4fa4fd
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-20 16:51:32 +0900

    enhigh coverage

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -12,7 +12,7 @@ func ConnectionDB() *sql.DB {
 	//　port := os.Getenv("DB_PORT")
 	//　name := os.Getenv("DB_NAME")
 	//　DB読み込み
-	//dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
+	//　dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
 	db, _ := sql.Open("mysql", "atsuser:atspass@tcp(localhost:3306)/kensyu_testing?parseTime=true")
 	return db
 }
