40a16cbed439c06c346496e78180ed6e51fe1d1a
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-21 13:14:48 +0900

    mdf

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -6,7 +6,8 @@ import (
 
 func ConnectionDB() *sql.DB {
 	//　dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
-	db, err := sql.Open("mysql", "root:Shinya0023@tcp(localhost:3306)/test_fix?parseTime=true")
+	//　db, err := sql.Open("mysql", "root:Shinya0023@tcp(localhost:3306)/test_fix?parseTime=true")
+	db, err := sql.Open("mysql", "atsuser:atspass@tcp(localhost:3306)/kensyu_testing?parseTime=true")
 	if err != nil {
 		return nil
 	}
