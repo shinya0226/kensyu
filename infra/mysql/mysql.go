58ac7bf7f7b599e6ba234d19b407d1d304103190
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-21 14:33:20 +0900

    mdf

--- infra/mysql/mysql.go
+++ infra/mysql/mysql.go
@@ -2,9 +2,18 @@ package mysql
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 )
 
 func ConnectionDB() *sql.DB {
-	db, _ := sql.Open("mysql", "atsuser:atspass@tcp(localhost:3306)/kensyu_testing?parseTime=true")
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+	//　DB読み込み
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
+	db, _ := sql.Open("mysql", dsn)
 	return db
 }
