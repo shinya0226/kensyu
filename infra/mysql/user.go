72454399a0ed29b0325b6cbf6aa07bf5ba83e90e
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-22 22:04:23 +0900

    list_create

--- infra/mysql/user.go
+++ infra/mysql/user.go
@@ -15,7 +15,7 @@ func NewUserRepository(db *sql.DB) entity.IUserRepository {
 
 func (ur *userRepository) FindSingleRow(email string) (entity.User, error) {
 	u := entity.User{}
-	if err := ur.db.QueryRow("SELECT * FROM users WHERE Email = ?", email).
+	if err := ur.db.QueryRow("SELECT * FROM user WHERE Email = ?", email).
 		Scan(&u.Email, &u.Password, &u.Name, &u.IsAdmin); err != nil {
 		//　Emailが合致しないとき
 		return u, err
