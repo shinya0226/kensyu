d2dc587a25c3c0e28eb64b04bf169d1f45dfc18f
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-26 15:18:16 +0900

    entity mdf

--- usecase/login.go
+++ usecase/login.go
@@ -14,16 +14,23 @@ func NewLoginUsecase(repo entity.IUserRepository) ILoginUsecase {
 }
 
 type ILoginUsecase interface {
-	Login(e entity.User) (entity.LoginFormat, error)
+	Login(e entity.User) (LoginFormat, error)
 }
 
-func (u *loginUsecase) Login(e entity.User) (entity.LoginFormat, error) {
-	var logfo entity.LoginFormat
+type LoginFormat struct {
+	Email       string `json:"email"`
+	Name        string `json:"name"`
+	IsAdmin     int    `json:"isAdmin"`
+	AccessToken string `json:"access_token"`
+}
+
+func (u *loginUsecase) Login(e entity.User) (LoginFormat, error) {
+	var logfo LoginFormat
 	//　該当するユーザーを抽出（found）
 	found, err := u.repo.FindSingleRow(e.Email)
 	//　Emailの合致確認
 	if err != nil {
-		return entity.LoginFormat{}, err
+		return LoginFormat{}, err
 	}
 
 	logfo.Email = found.Email
@@ -31,7 +38,7 @@ func (u *loginUsecase) Login(e entity.User) (entity.LoginFormat, error) {
 	//　Passwordの合致確認
 	err = verifyPassword(found.Password, e.Password)
 	if err != nil {
-		return entity.LoginFormat{}, err
+		return LoginFormat{}, err
 	}
 	logfo.Name = found.Name
 	logfo.IsAdmin = found.IsAdmin
