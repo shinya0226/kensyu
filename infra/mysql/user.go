70723d0c438d47ffb7cedca0e3d13cc745b46d91
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-28 12:38:11 +0900

    mdf

--- infra/mysql/user.go
+++ infra/mysql/user.go
@@ -18,7 +18,7 @@ func NewUserRepository(db *sql.DB) entity.IUserRepository {
 func (ur *userRepository) FindSingleRow(email string) (entity.User, error) {
 	table := os.Getenv("DB_TABLE")
 	u := entity.User{}
-	if err := ur.db.QueryRow("SELECT * FROM"+table+"WHERE Email = ?", email).
+	if err := ur.db.QueryRow("SELECT * FROM"+" "+table+" "+"WHERE Email = ?", email).
 		Scan(&u.Email, &u.Password, &u.Name, &u.IsAdmin); err != nil {
 		//　Emailが合致しないとき
 		return u, err
