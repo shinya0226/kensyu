4b332596eaf7c904a2079116fa6804170984694e
shinya0226 <shinya.yamamoto0033@gmail.com>
2023-12-29 12:23:13 +0900

    mdf

--- handler/handler.go
+++ handler/handler.go
@@ -79,7 +79,7 @@ func FetchAccounts() echo.HandlerFunc {
 		pagefirst := pageFirst * paging
 
 		table := os.Getenv("DB_TABLE")
-		rows, err := db.Query("SELECT * FROM ? LIMIT ?,?", table, pagefirst, paging)
+		rows, err := db.Query("SELECT * FROM"+" "+table+" "+"LIMIT ?,?", pagefirst, paging)
 		if err != nil {
 			return err
 		}
