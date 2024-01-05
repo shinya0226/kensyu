35963f615903b55c300c62d9766f278b67b9c108
shinya0226 <shinya.yamamoto0033@gmail.com>
2024-01-05 16:41:57 +0900

    login_cmp

--- migrations/users/000001_create_users_table.up.sql
+++ migrations/users/000001_create_users_table.up.sql
@@ -3,4 +3,4 @@ CREATE TABLE IF NOT EXISTS users(
     Password varchar(255) NOT NULL,
     Name varchar(255) NOT NULL,
     IsAdmin int NOT NULL
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
